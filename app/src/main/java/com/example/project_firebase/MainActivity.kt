f5170a82fffc2dd1dd78dc71c6235a9ce443bf92
Zaksapthawee <i-am-zak_189@hotmail.com>
2020-03-19 12:08:45 +0700

    input

--- app/src/main/java/com/example/project_firebase/MainActivity.kt
+++ app/src/main/java/com/example/project_firebase/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.project_firebase
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +14,16 @@ class MainActivity : AppCompatActivity() {
         if (supportActionBar != null)
             supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        //get firebase database reference
+        var mRootRef = FirebaseDatabase.getInstance().reference
+        var mTopicsRef = mRootRef.child("Topic")
+        //var mDetailsRef = mRootRef.child("Detail")
+
+        btn_save.setOnClickListener{
+            var inputtext = text_input.text.toString()
+            Toast.makeText(this, "Input : "+inputtext, Toast.LENGTH_LONG).show()
+        }
     }
 
-    var mRootRef = FirebaseDatabase.getInstance().reference
-    var mUsersRef = mRootRef.child("Topic")
 }
